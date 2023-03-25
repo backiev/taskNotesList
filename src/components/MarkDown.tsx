f7e2369e9bd24d5ed32835cf8a43ebdb786220fc
backiev <farkhat.bakiev@mail.ru>
2023-03-25 17:01:55 +0300

    Added all functional

--- src/components/MarkDown.tsx
+++ src/components/MarkDown.tsx
@@ -2,19 +2,44 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import { Typography } from '@mui/material';
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { Context } from '../Context';
+
+interface IStylesEdit {
+  height: number;
+  width: number;
+  backgroundColor: string;
+  color: string;
+}
+
+interface IStylesDefault {
+  fontWeight: string;
+  textDecoration: string;
+  fontStyle: string;
+}
+
+
 
 export const MarkDown: React.FC = () => {
   const {selectedNote,toggleEdit, giveEdit, editNote} = useContext(Context);
   const editMode = giveEdit();
   const note = selectedNote();
 
-  // const stylesS: <string>{} = {
-  //   fontWeight: 'bold',
-  //   fontStyle: 'italic',
-  //   textDecoration: 'underline',
-  // }
+  const stylesEdit: IStylesEdit = {
+    height: 400,
+    width: 400,
+    backgroundColor: 'white',
+    color: 'black',
+  }
+  const stylesDefault: IStylesDefault = {
+    fontWeight: 'none',
+    textDecoration: 'none',
+    fontStyle: 'none',
+  }
+
+  note[0].bold ? stylesDefault.fontWeight = 'bold' : stylesDefault.fontWeight = 'normal';
+  note[0].italic ? stylesDefault.fontStyle = 'italic' : stylesDefault.fontStyle = 'normal';
+  note[0].underline ? stylesDefault.textDecoration = 'underline' : stylesDefault.textDecoration = 'none';
 
   return (
     <Box>
@@ -25,15 +50,16 @@ export const MarkDown: React.FC = () => {
             aria-label="11px"
             minRows={3}
             value={note[0].text}
-            style={ { height: 400, width: 400, backgroundColor: 'white', color: 'black'} }
+            style={ stylesEdit }
             onChange={(e) => editNote(note[0].id, e.target.value)}
           />
         </Box>
       ) : (
         <Box>
           <Typography variant="h5">{note[0].title}</Typography>
-          <Typography variant="subtitle1" onClick={() => toggleEdit(true)} 
-            // style={}
+          <Typography variant="subtitle1" 
+            onClick={() => toggleEdit(true)} 
+            style={stylesDefault}
           >{note[0].text}</Typography>
         </Box>
       )}
